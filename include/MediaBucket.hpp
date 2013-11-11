76cc5146de34526c7c19ead02d3b2f99552acb84
eldhom <wass95@hotmail.se>
2013-11-11 13:02:06 +0100

    Made a new mediabucket

---
+++ include/MediaBucket.hpp
@@ -0,0 +1,39 @@
+#include<map>
+#include<vector>
+#include<SFML/Graphics.hpp>
+#include<SFML/Audio.hpp>
+
+
+#ifndef MEDIA_BUCKET_HPP
+#define MEDIA_BUCKET_HPP
+class MediaBucket {
+private:
+	//	Media
+	std::map<std::string, sf::Texture> 		textures;
+	std::map<std::string, sf::SoundBuffer>	soundbuffers;
+	std::mao<std::string, sf::Font>			fonts;
+
+	//	Media Names
+	std::vector<std::string> 				texture_names;
+	std::vector<std::string>				soundbuffer_names;
+	std::vector<std::string>				font_names;
+public:
+	bool									loadMedia(std::tring filename); 				//	Loads media with resource file
+	bool									loadTextureFromFile(std::string filename);		
+	bool									loadSoundBufferFromFile(std::string filename);
+	bool									loadFontFromFile(std::string filename);
+
+	int										getNumberOfTextures();
+	int										getNumberOfSoundBuffers();
+	int										getNumberOfFonts();
+
+	std::string&							getTextureName(int index);
+	std::string&							getSoundBufferName(int index);
+	std::string&							getFontName(int index);
+
+	sf::Texture&							getTexture(std::string name);
+	sf::SoundBuffer&						getSoundBuffer(std::string name);
+	sf::Font&								getFont(std::string name);
+
+};
+#endif
