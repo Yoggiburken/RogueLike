a2c866e9ac093ad797e65d6f5ae29da6cf8e44c9
eldhom <wass95@hotmail.se>
2013-11-12 13:36:32 +0100

    l

---
+++ mapmaker/include/MediaBucket.hpp
@@ -0,0 +1,38 @@
+#include<map>
+#include<vector>
+#include<SFML/Graphics.hpp>
+#include<SFML/Audio.hpp>
+
+
+#ifndef MEDIA_BUCKET_HPP
+#define MEDIA_BUCKET_HPP
+class MediaBucket {
+private:
+	//	Media
+	std::map<std::string, sf::Texture> 		textures;
+	std::map<std::string, sf::SoundBuffer>	soundbuffers;
+	std::map<std::string, sf::Font>			fonts;
+
+	//	Media Names
+	std::vector<std::string> 				texture_names;
+	std::vector<std::string>				soundbuffer_names;
+	std::vector<std::string>				font_names;
+public:
+	void									loadMedia(std::string filename); 				//	Loads media with resource file
+	bool									loadTextureFromFile(std::string filename);		
+	bool									loadSoundBufferFromFile(std::string filename);
+	bool									loadFontFromFile(std::string filename);
+
+	int										getNumberOfTextures() 				const;
+	int										getNumberOfSoundBuffers() 			const;
+	int										getNumberOfFonts() 					const;
+
+	std::string&							getTextureName(int index) 			const;
+	std::string&							getSoundBufferName(int index) 		const;
+	std::string&							getFontName(int index) 				const;
+
+	sf::Texture&							getTexture(std::string name) 		const;
+	sf::SoundBuffer&						getSoundBuffer(std::string name) 	const;
+	sf::Font&								getFont(std::string name) 			const;
+};
+#endif
